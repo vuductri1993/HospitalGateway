65645bf672b08559548db9cd2921bedba10149b9
Vu Duc Tri <trivd@mpos.vn>
2026-04-21 17:38:04 +0700

    Initial commit

---
+++ src/test/java/vn/trivd/hospitalgateway/HospitalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.trivd.hospitalgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
